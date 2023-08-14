ebcfb36eae06e2add0d8053a9c655a9196d96590
Hellagr <ctapjieu1991@gmail.com>
2023-08-14 21:02:37 +0300

    SOME LOGIC 2

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,25 +1,50 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import './Game.css';
+import { VscCircleLarge } from "react-icons/vsc";
+import { VscChromeClose } from "react-icons/vsc";
+import { VscBlank } from "react-icons/vsc";
+
+
 export default function Game() {
+    const [cells, setCells] = useState(new Array(9).fill(<VscBlank />));
 
-
-    const [cell, setCell] = useState(new Array(9).fill("*"));
+    if (cells[0].type.name == "VscChromeClose" || cells[1].type.name == "VscChromeClose" || cells[2].type.name == "VscChromeClose" || cells[3].type.name == "VscChromeClose" || cells[5].type.name == "VscChromeClose" || cells[6].type.name == "VscChromeClose" || cells[7].type.name == "VscChromeClose" || cells[8].type.name == "VscChromeClose" || cells[4].type.name == "VscBlank") {
+        cells[4] = <VscCircleLarge />
+    } else {
+        cells[6] = <VscCircleLarge />
+    }
 
     const setValue = (index) => {
-        const newValue = "1";
-        setCell((prevArray) => {
+        const newValue = <VscChromeClose />;
+        setCells((prevArray) => {
             return prevArray.map((item, element) =>
                 element === index ? prevArray[element] = newValue : item
-            )
-        })
+            );
+        });
     }
+
+    const winlose1 = cells[0].type.name == "VscChromeClose" && cells[1].type.name == "VscChromeClose" && cells[2].type.name == "VscChromeClose" ? "You are WIN!" : null
+    const winlose2 = cells[3].type.name == "VscChromeClose" && cells[4].type.name == "VscChromeClose" && cells[5].type.name == "VscChromeClose" ? "You are WIN!" : null
+    const winlose3 = cells[6].type.name == "VscChromeClose" && cells[7].type.name == "VscChromeClose" && cells[8].type.name == "VscChromeClose" ? "You are WIN!" : null
+    const winlose4 = cells[0].type.name == "VscChromeClose" && cells[4].type.name == "VscChromeClose" && cells[8].type.name == "VscChromeClose" ? "You are WIN!" : null
+    const winlose5 = cells[2].type.name == "VscChromeClose" && cells[4].type.name == "VscChromeClose" && cells[6].type.name == "VscChromeClose" ? "You are WIN!" : null
+
+
 
     return (
         <div>
-            <div>
-                {cell.map((e, index) => <div key={index} onClick={() => setValue(index)}>{e}</div>)}
+            {winlose1}
+            {winlose2}
+            {winlose3}
+            {winlose4}
+            {winlose5}
+            <div className="StyleGame">
+                {cells.map((value, index) => (
+                    <div key={index} className="cell" onClick={() => setValue(index)}>
+                        {value}
+                    </div>
+                ))}
             </div>
         </div>
-
-    )
-
+    );
 }
